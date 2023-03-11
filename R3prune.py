9f2214df0ba384c23bad56f6e0ae783527a4d9ae
Biondcbion <127460500+Biondcbion@users.noreply.github.com>
2023-03-11 10:55:02 -0500

    Add files via upload

---
+++ R3prune.py
@@ -0,0 +1,12 @@
+
+
+
+def pruning(CandidateItemset, previtemsets, length):
+    prunedSet = []
+    for candidate in CandidateItemset:
+        subsets = [candidate[:i] + candidate[i+1:] for i in range(length)]
+        if all(subset in previtemsets for subset in subsets):
+            prunedSet.append(candidate)
+    return prunedSet
+
+#canidateitemsets are the results from r2, previtems are all frequent itemsets, length is k-1
