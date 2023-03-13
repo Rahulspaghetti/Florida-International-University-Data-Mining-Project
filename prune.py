449a615a90d355d6e032eea89ccc16a99ba5bb50
Biondcbion <127460500+Biondcbion@users.noreply.github.com>
2023-03-12 21:23:59 -0400

    Update prune.py

--- prune.py
+++ prune.py
@@ -1,15 +1,19 @@
 
-
-def pruning(candidateitemsets, frequentitemsets, k):
-    pruneditemsets = []
-    frequentitemsetsset = set(map(tuple, frequentitemsets))  
-    for candidate in candidateitemsets:
-        for item in candidate:
-            subset = [x for x in candidate if x != item]
-            if tuple(subset) not in frequentitemsetsset: 
-                break
-        else:
-            pruneditemsets.append(candidate)
-    return pruneditemsets
+def pruning(candidateItemSets, frequentItemsets):
+    prunedItemSets = []
+    frequentItemsetsSet = set(map(tuple, frequentItemsets))
+    max_k = len(frequentItemsets[0])
+    for k in range(1, max_k + 1): 
+        frequentItemsetsK = [itemset for itemset in frequentItemsets if len(itemset) == k]
+        prunedItemsetsK = []
+        for candidate in candidateItemSets:
+            for item in candidate:
+                subset = tuple(sorted(set(candidate) - {item}))
+                if subset not in frequentItemsetsSet:
+                    break
+            else:
+                prunedItemsetsK.append(candidate)
+        prunedItemSets.extend(prunedItemsetsK)
+    return prunedItemSets
 
 
